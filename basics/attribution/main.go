499c1d5c2d214232ff4dd2f3fba205224249e97a
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-06-07 22:40:30 -0300

    Attribution variables in GoLang

---
+++ basics/attribution/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+	var b byte = 3
+	fmt.Println(b)
+
+
+	i := 3 //deduced type
+
+	i += 3 // i = i + 3
+	i -= 3 // i = i - 2
+	i /= 2 // i = i / 2
+	i *= 2 // i = i * 2
+	i %= 2 // i = i % 2
+
+	fmt.Println(i)
+
+	x, y := 1, 2
+	fmt.Println(x, y)
+
+	y, x = x, y
+	fmt.Println(x, y)
+
+}
+
