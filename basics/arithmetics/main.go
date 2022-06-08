49bbc118ebf7d244e9efd85c992620ae6eac7643
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-06-07 22:33:29 -0300

    arithmetics operations in GoLang

---
+++ basics/arithmetics/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	a := 3
+	b := 2
+
+	fmt.Println("Sum =>", a+b)
+	fmt.Println("Minus =>", a-b)
+	fmt.Println("Division =>", a/b)
+	fmt.Println("Multiply =>", a*b)
+	fmt.Println("Module =>", a%b)
+	
+	// bitwise
+	fmt.Println("E =>", a&b)
+	fmt.Println("OR =>", a|b)
+	fmt.Println("OR =>", a^b)
+
+	c := 3.0
+	d := 2.0
+
+	fmt.Println("Max ab =>", math.Max(float64(c), float64(d)))
+	fmt.Println("Max cd =>", math.Max(c, d))
+	fmt.Println("Min cd =>", math.Min(c, d))
+	fmt.Println("Pow cd =>", math.Pow(c, d))
+
+}
+
