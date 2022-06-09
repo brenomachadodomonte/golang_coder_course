282748944c54b42ff13c9407e941a439732ad2c2
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-06-09 20:58:35 -0300

    For control in GoLang

---
+++ controls/4-for/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	for i := 0; i<=20; i +=2 {
+		fmt.Printf("%d ", i)
+	}
+
+	for i := 1; i<=10; i++ {
+		if i%2 == 0 {
+			fmt.Printf("%d is even\n", i)
+		} else {
+			fmt.Printf("%d is odd\n", i)
+		}
+	}
+
+	// infinite loop
+	for {
+		fmt.Println("forever...")
+	}
+}
